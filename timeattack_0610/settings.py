a4904d7c2330c01b69eebad6b453eb92d7237cd1
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2022-06-15 11:54:24 +0900

    Update settings.py

--- timeattack_0610/settings.py
+++ timeattack_0610/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tww@ecycrmqq03ffc%zcs4y$afc6#9=wmqs=i4f!5bb5ii+-v&'
+#SECRET_KEY = 'django-insecure-tww@ecycrmqq03ffc%zcs4y$afc6#9=wmqs=i4f!5bb5ii+-v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
